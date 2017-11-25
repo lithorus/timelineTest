a8dd1283d0c24eb0e4829fbf9488aaef058dd317
lithorus <lithorus@gmail.com>
2017-11-25 22:46:29 +0100

    dump

--- timelineTest.py
+++ timelineTest.py
@@ -4,24 +4,56 @@ Module for testing a timeline
 '''
 
 import sys
+import re
 
 #from PySide import QtGui, QtUiTools, QtCore
-from PySide.QtGui import QApplication, QGraphicsRectItem, QColor, QBrush, QGraphicsScene, QGraphicsLineItem # pylint: disable=E0611,C0301
+from PySide.QtGui import QApplication, QGraphicsRectItem, QColor, QBrush, QPen, QGraphicsScene, QGraphicsLineItem, QGraphicsItemGroup # pylint: disable=E0611,C0301
 from PySide.QtUiTools import QUiLoader # pylint: disable=E0611
 from PySide.QtCore import Qt # pylint: disable=E0611
 
 frameWidth = 3
 trackHeight = 40
 
+def tc2frames(timecode, fps):
+    '''
+    Converts from timecode to framecount
+    '''
+    match = re.match(r"(\d{2}):(\d{2}):(\d{2}):(\d{2})", timecode)
+    if match is not None:
+        hours = int(match.group(1))
+        minutes = int(match.group(2))
+        seconds = int(match.group(3))
+        frames = int(match.group(4)) + seconds*fps + minutes*60*fps + hours*60*60*fps
+        return frames
+
 class clipItem(QGraphicsRectItem):
     '''
     Item for clips in the timeline
     '''
     fill = QBrush(QColor(127, 127, 127), Qt.SolidPattern)
+    pen = QPen()
     def __init__(self, startFrame, frames):
         QGraphicsRectItem.__init__(self)
         self.setRect(startFrame*frameWidth, 0, frames*frameWidth, trackHeight)
         self.setBrush(self.fill)
+        self.pen.setWidth(2)
+        self.setPen(self.pen)
+
+class timelineTrack(QGraphicsItemGroup):
+    '''
+    Timline track
+    '''
+    def __init__(self):
+        QGraphicsItemGroup.__init__(self)
+
+    def addClip(self, startFrame, frames):
+        '''
+        Add clip to the track
+        '''
+        clip = clipItem(startFrame, frames)
+        self.scene().addItem(clip)
+        self.addToGroup(clip)
+        return clip
 
 class timelineMarker(QGraphicsLineItem):
     '''
@@ -39,7 +71,6 @@ class timelineMarker(QGraphicsLineItem):
         trans.translate(frame*frameWidth, 0)
         self.setTransform(trans)
 
-
 class timelineTest(object):
     '''
     Main Class
@@ -53,22 +84,38 @@ class timelineTest(object):
         self.scene = QGraphicsScene()
         self.mainWindow.graphicsView.setScene(self.scene)
 
-        self.addClip(0, 50)
-        self.addClip(55, 50)
+        track = timelineTrack()
+        self.addTrack(track)
+        self.readEDL(track, 'sample.edl')
+        #track.addClip(0, 50)
+        #track.addClip(55, 50)
 
-        marker = timelineMarker()
-        marker.setFrame(40)
+        #marker = timelineMarker()
+        #marker.setFrame(40)
+        #self.scene.addItem(marker)
 
-        '''
-        trans = QTransform()
-        trans.translate(40, 0)
-        marker.setTransform(trans)
-        '''
-
-        self.scene.addItem(marker)
         self.mainWindow.show()
         self.app.exec_()
 
+    def readEDL(self, track, edlfile):
+        edlFile = open(edlfile)
+        for line in edlFile.readlines():
+            match = re.match(r"(\d{3})\s+\w+\s+\w+\s+\w+\s+(\d{2}:\d{2}:\d{2}:\d{2}) (\d{2}:\d{2}:\d{2}:\d{2}) (\d{2}:\d{2}:\d{2}:\d{2}) (\d{2}:\d{2}:\d{2}:\d{2})", line)
+            if match is not None:
+                editStart = tc2frames(match.group(2), 24)
+                editEnd = tc2frames(match.group(3), 24)
+                editLength = editStart - editEnd
+                track.addClip(editStart, editLength)
+
+        edlFile.close()
+
+    def addTrack(self, track):
+        '''
+        Add track to timeline
+        '''
+        self.scene.addItem(track)
+
+    """
     def addClip(self, startFrame, frames):
         '''
         Adds a clip to the timeline
@@ -77,5 +124,6 @@ class timelineTest(object):
 
         self.scene.addItem(clip)
         return clip
+    """
 
 timelineTest()
